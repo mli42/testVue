9a0e481056401d040ee919eda8991c08bf10a381
mli42 <mli@student.42.fr>
2021-07-24 23:32:28 +0200

    makeModale (+) Onglet menu to choose sub-app

--- frontend/src/components/App/App.vue
+++ frontend/src/components/App/App.vue
@@ -8,12 +8,10 @@
     <br/>
     <body id="subapps">
       <h1>This is a title</h1>
-      <counter></counter>
-      <mycanvas></mycanvas>
-      <button class="modaleBtnOn" @click="toggleModale">
-        Clique allez stp là!!!
-      </button>
-      <modale :showModale="showModale" :toggleModale="toggleModale"></modale>
+      <button class="onglet" @click="dynamicComp = 'counter'">Counter</button>
+      <button class="onglet" @click="dynamicComp = 'mycanvas'">Canvas</button>
+      <button class="onglet" @click="dynamicComp = 'makeModale'">Modale</button>
+      <component :is="dynamicComp"></component>
     </body>
   </div>
 </template>
@@ -21,25 +19,22 @@
 <script lang="ts">
 import Counter from '../Counter/Counter.vue'
 import Canvas from '../Canvas/Canvas.vue'
-import Modale from '../Modale/Modale.vue'
+import makeModale from '../makeModale/makeModale.vue'
 
 export default {
   name: 'App',
   data() {
     return {
-      showModale: false as boolean,
+      dynamicComp: "counter",
     };
   },
   methods: {
-    toggleModale(): void {
-      this.showModale = !this.showModale;
-    },
   },
   components: {
     counter: Counter,
     mycanvas: Canvas,
-    modale: Modale,
-  },
+    makeModale: makeModale,
+   },
 }
 </script>
 
---
+++ frontend/src/components/makeModale/makeModale.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button class="modaleBtnOn" @click="toggleModale">
+      Clique allez stp là!!!
+    </button>
+    <modale :showModale="showModale" :toggleModale="toggleModale"></modale>
+  </div>
+</template>
+
+<script lang="ts">
+import Modale from '../Modale/Modale.vue'
+
+export default {
+  name: 'makeModale',
+  data() {
+    return {
+      showModale: false as boolean,
+    };
+  },
+  methods: {
+    toggleModale(): void {
+      this.showModale = !this.showModale;
+    },
+  },
+  components: {
+   modale: Modale,
+  },
+}
+</script>
+
+<style scoped src="./makeModale.css">
+</style>
