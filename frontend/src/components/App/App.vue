11295725e6435b18ca2553dca31dd89ecfd42f67
mli42 <mli@student.42.fr>
2021-07-24 12:21:35 +0200

    Focus all + Logo centred

---
+++ frontend/src/components/App/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="myapp">
+    <header id="header" class="flex-container">
+      <img id="pong-logo" alt="pong-logo" src="../../assets/logo.png">
+      <h1>Deluxe Pong &#x2728;</h1>
+    </header>
+    <hr/> <br/>
+    <h1>This is a title</h1>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'App',
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  components: {
+  },
+}
+</script>
+
+<style src="./App.css">
+</style>
